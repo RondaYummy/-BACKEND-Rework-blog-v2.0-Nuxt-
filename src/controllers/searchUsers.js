08808da210d19d472edb0e19aa853d52457bb6ea
RondoYummy <basrers@ukr.net>
2021-09-19 17:37:08 +0300

    add search users

--- src/controllers/searchUsers.js
+++ src/controllers/searchUsers.js
@@ -1,23 +1,25 @@
+const models = require('../models/index');
+
 const searchUsers = async (req, res) => {
   // Пошук користувачів
   // /api/user/search/:searchValue
   try {
     const searchText = `${req.params.searchValue}`; // or some value from request.body
     const searchRegex = new RegExp(searchText, 'i');
-    const candidate = await User.find({
-      userName: searchRegex
-    }).limit(5)
+    const candidate = await models.User.find({
+      userName: searchRegex,
+    }).limit(5);
     res.status(200).json({
       message: 'Користувачі були знайдені.',
       candidate,
     });
   } catch (e) {
     res.status(500).json({
-      message: 'Щось пішло не так, попробуйте ще раз1.'
+      message: 'Щось пішло не так, попробуйте ще раз1.',
     });
   }
 };
 
 module.exports = {
-  searchUsers
+  searchUsers,
 };
